69b527909b2b8b067c20f8b3a457115c9e61a28d
Kimonas Sotirchos <kimwnasptd@192.168.1.12>
2023-06-03 23:36:17 +0300

    remove sudo from docker
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@192.168.1.12>

--- scripts/docker/docker.sh
+++ scripts/docker/docker.sh
@@ -7,14 +7,14 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
 apt-get update
 apt-get install -y ca-certificates curl gnupg
 
-sudo install -m 0755 -d /etc/apt/keyrings
+install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
