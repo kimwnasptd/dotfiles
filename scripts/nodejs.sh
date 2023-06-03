9da4eeb50e3ecde56f455955c8dbbd094ab35ef9
Kimonas Sotirchos <kimwnasptd@192.168.1.12>
2023-06-03 22:56:28 +0300

    Explicitly -y in apt install for nodejs

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-apt install nodejs  # needs sudo
-apt install npm  # needs sudo
+apt install -y nodejs  npm # needs sudo
 
 echo "Setting npm dir to user home..."
 mkdir -p ~/.npm-global
