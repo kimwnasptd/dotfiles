348903c358ce59f8d65f70d9301ae7d31f6aa065
Kimonas Sotirchos <kimwnasptd@192.168.1.12>
2023-06-11 16:33:57 +0300

    docker: Don't hardcode amd64
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@192.168.1.12>

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository -y "deb https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 
 sudo apt install -y docker-ce
