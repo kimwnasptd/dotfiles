422185b530ff0b1d487f0a4b0a0013518e466a8f
Kimonas Sotirchos <kimwnasptd@gmail.com>
2023-06-11 14:32:39 +0000

    golang: Conditionally download ARM binary
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@gmail.com>

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -1,7 +1,17 @@
 set -o xtrace
 cd /tmp
 
-wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
+arch="$(arch)"
+case "$arch" in
+    'aarch64')
+        url=https://go.dev/dl/go1.20.2.linux-arm64.tar.gz
+        ;;
+    'x86_64')
+        url=https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
+        ;;
+esac
+
+wget $url -O golang.tar.gz
 
 echo "Extracting the golang tar..."
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz  # needs sudo
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf golang.tar.gz  # needs sudo
