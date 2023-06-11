8d47d271dffb699732f9b088b67d89bd6d137455
Kimonas Sotirchos <kimwnasptd@192.168.1.12>
2023-06-11 17:06:32 +0300

    docker: Install using snap
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@192.168.1.12>

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,14 +2,18 @@
 set -o xtrace
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+#sudo apt update
+#sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#apt-cache policy docker-ce
 
-sudo apt install -y docker-ce
+#sudo apt install -y docker-ce
+
+sudo snap refresh
+sudo snap install docker
+
 sudo groupadd docker
 sudo usermod -aG docker ${USER}
 
