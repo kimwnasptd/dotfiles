a120887ee03ea020084d8aae5b87302901cd6905
Kimonas Sotirchos <kimwnasptd@192.168.1.12>
2023-06-11 16:45:01 +0300

    docker: Add back amd64
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@192.168.1.12>

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,16 +6,12 @@ sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository -y "deb https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 
 sudo apt install -y docker-ce
 sudo groupadd docker
 sudo usermod -aG docker ${USER}
-
-# remove need for sudo
-#sudo usermod -aG docker $USER
-#newgrp docker
 
 # copy wsl config
 #cp ${SCRIPT_DIR}/configs/wsl-docker-daemon.json /etc/docker/daemon.json
