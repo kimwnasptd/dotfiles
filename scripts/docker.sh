3956d32c9ea4e7619feb02dfb6bfc2ed4fcf9fae
Kimonas Sotirchos <kimwnasptd@192.168.1.12>
2023-06-11 12:28:08 +0300

    docker: The gift that keeps on giving
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@192.168.1.12>

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,15 +5,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 
-sudo apt install docker-ce
+sudo apt install -y docker-ce
+sudo groupadd docker
 sudo usermod -aG docker ${USER}
 
 # remove need for sudo
-#sudo groupadd docker
 #sudo usermod -aG docker $USER
 #newgrp docker
 
