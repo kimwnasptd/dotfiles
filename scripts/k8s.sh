4e42a03e5c5ff8fdfa7b9a76745a3ab49d885203
Kimonas Sotirchos <kimwnasptd@gmail.com>
2023-09-02 21:19:48 +0300

    scripts(k8s): Use arch for binaries
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@gmail.com>

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -1,7 +1,20 @@
-# kubectl
 set -o xtrace
+
+arch="$(arch)"
+case "$arch" in
+    'aarch64')
+        kustomize_url=https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.0.1/kustomize_v5.0.1_linux_arm64.tar.gz
+        kubectl_url=https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl
+        ;;
+    'x86_64')
+        kustomize_url=https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.0.1/kustomize_v5.0.1_linux_amd64.tar.gz
+        kubectl_url=https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+        ;;
+esac
+
+# kubectl
 cd /tmp
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO $kubectl_url
 chown $USER kubectl
 chmod +x kubectl
 mv kubectl ~/.local/bin/kubectl
@@ -10,14 +23,14 @@ mv kubectl ~/.local/bin/kubectl
 /usr/local/go/bin/go install sigs.k8s.io/kind@v0.17.0
 
 # kustomize 5.0.1
-wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.0.1/kustomize_v5.0.1_linux_amd64.tar.gz
-tar xzvf kustomize_v5.0.1_linux_amd64.tar.gz
+wget $kustomize_url
+tar xzvf *.tar.gz
 chown $USER kustomize
 chmod +x kustomize
-mv kustomize ~/.local/bin/kustomize-5
+mv kustomize ~/.local/bin/kustomize
 
 # kustomize 3.2
-wget https://github.com/kubernetes-sigs/kustomize/releases/download/v3.2.0/kustomize_3.2.0_linux_amd64
-chown $USER kustomize_3.2.0_linux_amd64
-chmod +x kustomize_3.2.0_linux_amd64
-mv kustomize_3.2.0_linux_amd64 ~/.local/bin/kustomize
+#wget https://github.com/kubernetes-sigs/kustomize/releases/download/v3.2.0/kustomize_3.2.0_linux_amd64
+#chown $USER kustomize_3.2.0_linux_amd64
+#chmod +x kustomize_3.2.0_linux_amd64
+#mv kustomize_3.2.0_linux_amd64 ~/.local/bin/kustomize
