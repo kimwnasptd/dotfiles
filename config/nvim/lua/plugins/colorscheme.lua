4c1d3e83e0a0d8d806e991979531caca2eacadb6
Kimonas Sotirchos <kimwnasptd@gmail.com>
2024-12-28 18:23:06 +0200

    nvim: More colorschemes

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,33 @@
+-- https://dotfyle.com/neovim/colorscheme/top
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme "catppuccin"
+    end
+  },
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      require('onedark').setup {
+          style = 'darker'
+      }
+      -- require('onedark').load()
+    end
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    config = function()
+      vim.cmd.colorscheme "vscode"
+    end
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      -- vim.cmd.colorscheme "kanagawa"
+    end
+  }
+}
