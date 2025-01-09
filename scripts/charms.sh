31709b8e19780cad8a19c4b12913e11f5cb80463
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-01-09 14:16:20 +0200

    scripts: Fix lxd docker issues

--- scripts/charms.sh
+++ scripts/charms.sh
@@ -3,3 +3,10 @@ set -o xtrace
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
 sudo apt install python3-poetry
+sudo snap install charmcraft --classic
+getent group lxd | grep -qwF "$USER" || sudo usermod -aG lxd "$USER"
+
+# LXD and docker
+sudo iptables -I DOCKER-USER -i lxdbr0 -j ACCEPT
+sudo iptables -I DOCKER-USER -o lxdbr0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
