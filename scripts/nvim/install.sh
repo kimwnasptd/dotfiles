2db44aeed2241a80f8945222e1d407254c3dba9a
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-01-09 17:51:39 +0200

    nvim: Pin version and install ripgrep

--- scripts/nvim/install.sh
+++ scripts/nvim/install.sh
@@ -11,9 +11,11 @@ sudo apt-get install -y \
         ninja-build \
         pkg-config \
         unzip \
-        gettext
+        gettext \
+        ripgrep
 
 cd /tmp
 git clone https://github.com/neovim/neovim
+git checkout v0.10.3
 cd neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
