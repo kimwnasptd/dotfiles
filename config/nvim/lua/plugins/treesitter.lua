59d6688b8ce5533f22c0e1f903cd47b141b827ed
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-01-10 14:51:40 +0200

    nvim: Default parsers for treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,17 @@ return {
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = true,
+        ensure_installed = {
+          "python",
+          "bash",
+          "markdown",
+          "markdown_inline",
+          "vim",
+          "vimdoc",
+          "gitcommit",
+          "gitignore",
+          "go"
+        },
         highlight = { enable = true },
         indent = { enable = true },
       })
