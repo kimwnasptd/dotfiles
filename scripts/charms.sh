227cb5d31e9ae9c05cce6a3412bbb6c8fecccf18
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-04-09 13:59:34 +0300

    scripts: Install juju

--- scripts/charms.sh
+++ scripts/charms.sh
@@ -1,12 +1,15 @@
 set -o xtrace
 
+# python 3.8 and poetry
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
 sudo apt install python3-poetry
+
+# Canonical tools
 sudo snap install charmcraft --classic
+sudo snap install juju
 getent group lxd | grep -qwF "$USER" || sudo usermod -aG lxd "$USER"
 
 # LXD and docker
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 $SCRIPT_DIR/lxd-docker.sh
