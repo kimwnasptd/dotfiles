c48a82e78b7e2d5c246e1eddd63b6b3e8cd01fca
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-05-04 12:56:30 +0300

    scripts: Use docker's commands for installation

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,18 +2,28 @@
 set -o xtrace
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-#sudo apt update
-#sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL \
+	https://download.docker.com/linux/ubuntu/gpg \
+	-o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-#apt-cache policy docker-ce
+# Add apt repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-#sudo apt install -y docker-ce
+# install docker
+sudo apt install -y \
+	docker-ce docker-ce-cli \
+	containerd.io \
+	docker-buildx-plugin docker-compose-plugin
 
-sudo snap refresh
-sudo snap install docker
-
+# setup non-root usage
 sudo groupadd docker
 sudo usermod -aG docker ${USER}
 
