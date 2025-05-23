a104d6915b63d88deec5a2833737f50e3fbd8510
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-05-23 18:32:21 +0300

    scripts: Use main as default git branch

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,3 +5,4 @@ sudo apt install git -y
 git config --global user.email "kimwnasptd@gmail.com"
 git config --global user.name "Kimonas Sotirchos"
 git config --global commit.gpgsign true
+git config --global init.defaultBranch main
