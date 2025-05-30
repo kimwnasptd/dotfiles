102ba038aa5983c8691d5803698f4b3cdbcff743
Kimonas Sotirchos <kimwnasptd@gmail.com>
2025-05-30 15:08:22 +0300

    nvim: don't show blame lines

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
   "lewis6991/gitsigns.nvim",
   event = "BufReadPre",
   opts = {
-    current_line_blame = true,
+    current_line_blame = false,
     current_line_blame_opts = {
       virt_text = true,
       virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
