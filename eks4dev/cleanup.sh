a0d87a088352c9d33169d38545291c06abc3010d
Kimonas Sotirchos <kimonas.sotirchos@canonical.com>
2025-12-28 23:06:44 +0200

    chore: clean up older arrikto files

--- eks4dev/cleanup.sh
+++
@@ -1,4 +0,0 @@
-rm -rf *.pyc
-rm -rf preseed.env
-rm -rf preseed.py
-rm -rf dockerconfig.json
--- eks4dev/encrypt_env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-ENV_FILE=preseed.env
-
-echo "Encrypting env..."
-rm -f "${ENV_FILE}.gpg"
-gpg --armor --batch --symmetric --output "${ENV_FILE}.gpg" $ENV_FILE
