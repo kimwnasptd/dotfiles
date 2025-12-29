080794442531afdbac3748dec9348a4e5a68632d
Kimonas Sotirchos <kimonas.sotirchos@canonical.com>
2025-12-29 15:55:21 +0200

    nvim: update telescope theme

--- nvim-config/lua/plugins/telescope.lua
+++ nvim-config/lua/plugins/telescope.lua
@@ -4,6 +4,18 @@ return {
     tag = 'v0.2.0',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+      -- custom theme
+      local pickers = {
+        find_files = {
+          theme = "dropdown",
+        }
+      }
+
+      require('telescope').setup{
+        pickers = pickers,
+      }
+
+      -- keymaps like ctrlp
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
