9821b1487c048229dc73302d25bfcef8ca251703
Kimonas Sotirchos <kimonas.sotirchos@canonical.com>
2025-12-30 15:14:13 +0200

    nvim: whitter text in colorscheme

--- nvim-config/lua/config/colorscheme.lua
+++ nvim-config/lua/config/colorscheme.lua
@@ -1,43 +1,53 @@
+local white_msg = "#BFC9F5"
+local white_text = "#C8D3F5"
+local yellow = "#FFC777"
+local purple = "#C099FF"
+local deep_blue = "#65BCFF"
+local red = "#FF757F"
+
 local setCommonSettings = function()
   vim.cmd.colorscheme "tokyonight"
 
-  -- async should be same as def (purple)
-  vim.api.nvim_set_hl(0, "@keyword", { fg = "#C099FF" })
+  -- whitter text
+  vim.api.nvim_set_hl(0, "MsgArea", { fg = white_msg })
+  vim.api.nvim_set_hl(0, "NeoTreeFileName", { fg = white_msg })
+  -- async should be same as def
+  vim.api.nvim_set_hl(0, "@keyword", { fg = purple })
 
-  -- variable members and func params should be same as variables (white)
-  vim.api.nvim_set_hl(0, "@variable.member", { fg = "#C8D3F5" })
-  vim.api.nvim_set_hl(0, "@variable.parameter", { fg = "#C8D3F5" })
+  -- variable members and func params should be same as variables
+  vim.api.nvim_set_hl(0, "@variable.member", { fg = white_text })
+  vim.api.nvim_set_hl(0, "@variable.parameter", { fg = white_text })
 
   -- docstrings (yellow)
-  vim.api.nvim_set_hl(0, "@string.documentation", { fg = "#FFC777" })
+  vim.api.nvim_set_hl(0, "@string.documentation", { fg = yellow })
 end
 
 local zenBlueFn = function()
   setCommonSettings()
 
-  -- import modules, class constructors, func arg types (blue)
-  vim.api.nvim_set_hl(0, "@module", { fg = "#65BCFF" })
-  vim.api.nvim_set_hl(0, "@constructor", { fg = "#65BCFF" })
-  vim.api.nvim_set_hl(0, "@Type", { fg = "#65BCFF" })
+  -- import modules, class constructors, func arg types
+  vim.api.nvim_set_hl(0, "@module", { fg = deep_blue })
+  vim.api.nvim_set_hl(0, "@constructor", { fg = deep_blue })
+  vim.api.nvim_set_hl(0, "@Type", { fg = deep_blue })
 end
 
 local brightLightsFn = function()
   setCommonSettings()
 
-  -- import modules, class constructors, func arg types (red)
-  vim.api.nvim_set_hl(0, "@module", { fg = "#FF757F" })
-  vim.api.nvim_set_hl(0, "@constructor", { fg = "#FF757F" })
-  vim.api.nvim_set_hl(0, "@type", { fg = "#FF757F" })
+  -- import modules, class constructors, func arg types
+  vim.api.nvim_set_hl(0, "@module", { fg = red })
+  vim.api.nvim_set_hl(0, "@constructor", { fg = red })
+  vim.api.nvim_set_hl(0, "@type", { fg = red })
 
-  -- self same as def (purple)
-  vim.api.nvim_set_hl(0, "@variable.builtin", { fg = "#C099FF" })
+  -- self same as def
+  vim.api.nvim_set_hl(0, "@variable.builtin", { fg = purple })
 end
 
 local zenBlueNightFn = function()
   zenBlueFn()
 
-  -- self same as def (purple)
-  vim.api.nvim_set_hl(0, "@variable.builtin", { fg = "#C099FF" })
+  -- self same as def
+  vim.api.nvim_set_hl(0, "@variable.builtin", { fg = purple })
 end
 
 -- Custom User Commands for TokyoNight themes
