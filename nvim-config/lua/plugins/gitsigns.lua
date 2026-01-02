ef2d758e1d7c27b59f555b27435cea074f1e25e7
Kimonas Sotirchos <kimonas.sotirchos@canonical.com>
2026-01-02 18:26:11 +0200

    nvim: keymaps for git diff

--- nvim-config/lua/plugins/gitsigns.lua
+++ nvim-config/lua/plugins/gitsigns.lua
@@ -1,22 +1,27 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = "BufReadPre",
-  opts = {
-    current_line_blame = false,
-    current_line_blame_opts = {
-      virt_text = true,
-      virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
-      delay = 0,
-      ignore_whitespace = false,
-    },
-    signs = {
-      add = { text = "│" },
-      change = { text = "│" },
-      delete = { text = "│" },
-      topdelete = { text = "│" },
-      changedelete = { text = "│" },
-      untracked = { text = "│" },
-    },
-  },
+  config = function()
+    local opts = {
+      current_line_blame = false,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+        delay = 0,
+        ignore_whitespace = false,
+      },
+      signs = {
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "│" },
+        topdelete = { text = "│" },
+        changedelete = { text = "│" },
+        untracked = { text = "│" },
+      },
+    }
+
+    vim.keymap.set("n", "<Leader>gd", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set("n", "<Leader>gbt", ":Gitsigns toggle_current_line_blame<CR>", {})
+  end
 }
 
