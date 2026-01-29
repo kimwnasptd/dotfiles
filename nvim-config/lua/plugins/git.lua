331f112962c203852dc96007ab23a7b51118aee0
Kimonas Sotirchos <kimonas.sotirchos@canonical.com>
2026-01-29 19:06:27 +0200

    nvim: refactor git plugins

---
+++ nvim-config/lua/plugins/git.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "tpope/vim-fugitive"
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    config = function()
+      local opts = {
+        current_line_blame = false,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+          delay = 0,
+          ignore_whitespace = false,
+        },
+        signs = {
+          add = { text = "│" },
+          change = { text = "│" },
+          delete = { text = "│" },
+          topdelete = { text = "│" },
+          changedelete = { text = "│" },
+          untracked = { text = "│" },
+        },
+      }
+      require('gitsigns').setup(opts)
+
+      vim.keymap.set("n", "<Leader>gd", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<Leader>gbt", ":Gitsigns toggle_current_line_blame<CR>", {})
+    end
+  }
+}
