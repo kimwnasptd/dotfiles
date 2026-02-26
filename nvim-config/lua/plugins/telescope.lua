7a866f4e2cd7d07262bcc2b8afb1435504875116
Kimonas Sotirchos <kimonas.sotirchos@mistral.ai>
2026-02-26 10:52:30 +0100

    nvim: live grep args

--- nvim-config/lua/plugins/telescope.lua
+++ nvim-config/lua/plugins/telescope.lua
@@ -4,17 +4,26 @@ return {
     tag = 'v0.2.0',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      { "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0"}
     },
     config = function()
       local telescope = require('telescope')
       telescope.setup({})
+
       telescope.load_extension('fzf')
+      telescope.load_extension("live_grep_args")
 
       -- keymaps like ctrlp
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+      -- live_grep (args)
+      vim.keymap.set(
+        "n",
+        "<leader>fg",
+        ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
     end
   },
 }
